9018acb86ee564ec3d1e4b4ea0988f0aa8d50d6b
yuinaTanaka <yuinatanaka@yuinaTanakanoMacBook-Air.local>
2025-08-28 09:23:26 +0900

    hello

--- TDD/MoneyTest.py
+++ TDD/MoneyTest.py
@@ -2,12 +2,14 @@ import unittest
 from money import Money
 
 class MoneyTest(unittest.TestCase):
+
     # ドルを掛け算するテスト
     def testMultiplication(self):
         five = Money.dollar(5)
         #5ドルに3を掛けた結果が15ドルと等しいかテスト
         self.assertEqual(Money.dollar(10), five.times(2))
-       
+        self.assertEqual(Money.dollar(15), five.times(3))
+
     # 通貨が同じか、違うかを比較するテスト
     def testEquality(self):
         # 5ドルと6ドルは等しくない
@@ -15,15 +17,22 @@ class MoneyTest(unittest.TestCase):
         # 5フランと6フランは等しくない
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
         # 5フランと5ドルは等しくない
-        self.assertNotEqual(Money.dollar(5), Money.franc(5))
+        self.assertEqual(Money.dollar(5), Money.franc(5))
+
+        self.assertNotEqual(Money.franc(5), Money.franc(6))
+
+        self.assertNotEqual(Money.franc(5), Money.dollar(5))
 
     # フランを掛け算するテスト  
     def testFrancMultiplication(self):
         five = Money.franc(5)
-        # フランに3を掛けた結果が10フランと等しいか（このテストは失敗する
-        self.assertEqual(Money.franc(10), five.times(3))
+        self.assertEqual(Money.franc(10), five.times(2))
         # フランに3を掛けた結果が15フランと等しいか（このテストは成功する）
         self.assertEqual(Money.franc(15), five.times(3))
+
+    def testCurrency(self):
+        self.assertEquals("USD",Money.dollar(1).currency())
+        self.assertEquals("CHF",Money.franc(1).currency())
 
 if __name__ == '__main__':
     unittest.main()
--- TDD/money.py
+++ TDD/money.py
@@ -3,12 +3,18 @@ from abc import ABC, abstractmethod
 #ドルとフランに共通する親クラス
 class Money(ABC):
     #共通のフィールド（amount）
-    def __init__(self, amount):
+    def __init__(self, amount:int,currency:str):
         self.amount = amount
+        self._currency = currency
+
     @abstractmethod
     #金額を掛ける（サブクラスで実装が必要な抽象メソッド）
     def times(self, multiplier: int):
         pass
+
+    @abstractmethod
+    def currency(self):
+        return self._currency
     
     #共通のメソッド（金額と通貨が等しいか比較する）
     #引数の説明
@@ -25,24 +31,25 @@ class Money(ABC):
     #Dollarクラスのインスタンスを生成して返す
     @classmethod
     def dollar(cls, amount: int):
-        return Dollar(amount)
+        return Dollar(amount,"USD")
     #クラスメソッド
     #Francクラスのインスタンスを生成して返す
     @classmethod
     def franc(cls, amount: int):
-        return Franc(amount)
+        return Franc(amount,"CHF")
 
 #実装クラス
 class Dollar(Money):
+    def __init__(self,amount:int,currency:str):
+        super().__init__(amount,currency)
+    
     def times(self, multiplier: int):
-        return Dollar(self.amount * multiplier)
-#クラスメソッドとインスタンスメソッドを比較
-    def __eq__(self, other):
-        return isinstance(other, Dollar) and self.amount == other.amount
+        return Money.dollar(self.amount * multiplier)
+    
 #実装クラス
 class Franc(Money):
+    def __init__(self,amount,currency):
+        super().__init__(amount,currency)
+    
     def times(self, multiplier: int):
         return Franc(self.amount * multiplier)
-#クラスメソッドとインスタンスメソッドを比較
-    def __eq__(self, other):
-        return isinstance(other, Franc) and self.amount == other.amount
